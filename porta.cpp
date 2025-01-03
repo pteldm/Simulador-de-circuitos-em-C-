10cc8b52702861bdc80ec5b0e78a84ea17322cc1
Pedro Thiago E. de Lima <166128053+pteldm@users.noreply.github.com>
2025-01-03 20:23:37 -0300

    Class PortaNOR completa

--- porta.cpp
+++ porta.cpp
@@ -9,12 +9,42 @@
 //
 // FALTA IMPLEMENTAR
 //
+bool PortaNOT::simular(const std::vector<bool3S>& in_port)
+{
+    bool3S prov = bool3S::UNDEF;
+    if(in_port.size()>0 && in_port.size() == this->Nin_port)
+    {
+        for(int i=0; i<in_port.size(); ++i)
+        {
+            prov = ~(in_port[i]);
+        }
+        out_port = prov;
+        return true;
+    }
+    out_port = prov;
+    return false;
+}
 
 /// Porta AND
 
 //
 // FALTA IMPLEMENTAR
 //
+bool PortaAND::simular(const std::vector<bool3S>& in_port)
+{
+    bool3S prov = bool3S::UNDEF;
+    if(in_port.size()>0 && in_port.size() == this->Nin_port)
+    {
+        for(int i=0; i<in_port.size()-1;++i)
+        {
+            prov = in_port[i]&in_port[i+1];
+        }
+        out_port = prov;
+        return true;
+    }
+    out_port = prov;
+    return false;
+}
 
 /// Porta NAND
 
@@ -22,17 +52,60 @@
 // FALTA IMPLEMENTAR
 //
 
+bool PortaNAND::simular(const std::vector<bool3S> &in_port)
+{
+    bool3S prov = bool3S::UNDEF;
+    if(in_port.size()>0 && in_port.size()== this->Nin_port)
+    {
+        for(int i=0; i<in_port.size()-1; ++i)
+        {
+            prov = ~(in_port[i]&in_port[i+1]);
+        }
+        out_port = prov;
+        return true;
+    }
+    out_port = prov;
+    return false;
+}
+
 /// Porta OR
 
 //
 // FALTA IMPLEMENTAR
 //
+bool PortaOR::simular(const std::vector<bool3S> &in_port)
+{
+    bool3S prov = bool3S::UNDEF;
+    if(in_port.size()>0 && in_port.size() == this->Nin_port)
+    {
+        for(int i=0; i<in_port.size()-1; ++i)
+        {
+            prov = in_port[i]|in_port[i+1];
+        }
+        this->out_port=prov;
+    }
+}
 
 /// Porta NOR
 
 //
 // FALTA IMPLEMENTAR
 //
+bool PortaNOR::simular(const std::vector<bool3S> &in_port)
+{
+    bool3S prov = bool3S::UNDEF;
+    if(in_port.size()>0 && in_port.size()==Nin_port)
+    {
+        for(int i=0;i<in_port.size()-1; ++i)
+        {
+            prov = ~(in_port[i]|in_port[i+1]);
+        }
+        out_port=prov;
+        return true;
+    }
+    out_port=prov;
+    return false;
+}
 
 /// Porta XOR
 
